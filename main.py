d9d3c3f08e161e32a92ec5e81f3fdcf7d036d3e7
TezzaLuvsPigeons <terenceportelli2019@gmail.com>
2023-03-20 06:38:19 +0100

    Commit 3

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ The first line in his file is his indexes corresponding to each already entered 
 The rest of the file is the transactions.
 
 To do:
-> Fix method names (make them conventional)
 > Add comments.
 > Make the program more user-friendly.
 > Make the transaction method functional (therefore the user can add or take money from his balance)
@@ -18,19 +17,12 @@ from tkinter import *
 import json
 from tkcalendar import Calendar
 from datetime import *
-
-# 1 is row number, and the items in the list are the data.
-# The values in this variable are set depending on which account the user signs in to.
-date_lines = dict()  # "lines" is a variable which contains the line number which is corresponding to a saved date.
-# This is temporary, so I see how the variables will be structured.
-data = [{1: ["£25", "50", "89"]}, {2: ["£30", "25", "100"]}]
-balance = {"Terence2": 1000, "Terence3": 1500}
-
+#CURRENTLY WORKING ON SUBMIT TRANSACTION FUNCTIONALITY
 # Loads all accounts (their usernames and passwords)
 try:
     with open("accounts", "r") as file:
         data = file.readlines()
-except:
+except Exception:
     print("Something went wrong!")
 
 # The program tries to get each account's username and password from the accounts text file, to a dictionary variable.
@@ -49,6 +41,8 @@ except IndexError:
     # If the text file is empty, an empty dictionary is created.
     balances = dict()
 
+user_data = [{500: "Food", 400: "Accessories"}]
+
 main_menu_window = Tk()
 
 # The current account whose data is being used, and manipulated.
@@ -57,10 +51,11 @@ current_account = StringVar()
 # The line in the text file which is currently selected and will be manipulated.
 selected_line = IntVar()
 
-# The account which is currently signed into.
+# The account which is currently signed in to.
 selected_account = StringVar()
 
-def checkDate(date):
+
+def check_date(date):
     # A list of all the dates which have already been entered / created before is created.
     dates_entered = [i for i in date_lines.values()]
 
@@ -86,15 +81,16 @@ def checkDate(date):
 
 
 # The selected date in the calendar is submitted.
-def submitDate(calendar, date_label):
-    checkDate(calendar.get_date())
+def submit_date(calendar, date_label):
+    check_date(calendar.get_date())
 
-    #The label in the account menu is updated to the newly selected date.
+    # The label in the account menu is updated to the newly selected date.
     date_label.config(text=f"Selected date: {calendar.get_date()}")
     print(selected_line.get())
 
+
 # The calendar is opened
-def openCalendar(date_label):
+def open_calendar(date_label):
 
     calendar_menu = Toplevel()
     calendar_menu.title("Choose Date")
@@ -103,54 +99,73 @@ def openCalendar(date_label):
     calendar = Calendar(calendar_menu, date_pattern="dd/mm/yy")
     calendar.pack()
     submit_button = Button(calendar_menu, text="Submit Date", font=("Arial", 25),
-                           command=lambda date=calendar, label=date_label: submitDate(calendar, label))
+                           command=lambda date=calendar, label=date_label: submit_date(calendar, label))
     submit_button.pack()
 
-
-# The user can either take money from his balance or put money in his balance
-def transaction():
-    transaction_window = Toplevel()
-
-    transaction_frame = Frame(transaction_window)
-    transaction_frame.pack()
-    Label(transaction_frame, text="Money spent: ", font=("Arial", 20)).grid(row=0, column=0)
-    money_entry = Entry(transaction_frame, font=("Arial", 20))
-    money_entry.grid(row=0, column=1)
-
-
-#The program grabs the date from the account's file.
-def grabUserData(username):
+# The program grabs the date from the account's file.
+def grab_user_data(username):
 
     global date_lines
     try:
         with open(f"{username}.txt", "r") as file:
-            #Grabs all the data from the account's file.
+            # Grabs all the data from the account's file.
             user_data = file.readlines()
             try:
-                #The first line in the file is the corresponding line numbers and dates.
+                # The first line in the file is the corresponding line numbers and dates.
                 date_lines_json = json.loads(user_data[0])
                 date_lines = dict(date_lines_json)
             except IndexError:
                 # If the text file is empty, an empty dictionary is created.
                 date_lines = dict()
 
-    except:
+    except Exception:
         print("Something went wrong!")
 
-    #The currently signed in account is set so the program knows which text file to be using.
+    # The currently signed in account is set so the program knows which text file to be using.
     selected_account.set(username)
 
 
+#FIX ERROR ON LINE 136
+#Then I need to make it so that the total money transacted in the selected date is calculated (grabbing all the keys from
+#the user_data dictionary and adding them to each other). Then I need to modify the balance label accordingly to the
+#new balance.
+#Then I need to make the user_data variable savable, and loadable in/from the account text file
+def submit_transaction(transaction_entry, category_entry, transaction_label, balance_label):
+
+    balances[selected_account.get()] += transaction_entry.get()
+    #When this line is executed an error happens (IndexError: list index out of range)
+    user_data[selected_line.get()].update({transaction_entry.get() : category_entry.get()})
+    money_transacted = 0
+    transaction_label.config(text=f"Money spent on {date_lines.get(selected_line)}: ")
+
+
+# The user can either take money from his balance or put money in his balance
+def open_transaction_menu(transaction_label, balance_label):
+
+    transaction_window = Toplevel()
+    transaction_window.geometry("300x300")
+
+    transaction_frame = Frame(transaction_window)
+    transaction_frame.pack()
+    Label(transaction_frame, text="Money spent: ", font=("Arial", 20)).grid(row=0, column=0)
+    money_entry = Entry(transaction_frame, font=("Arial", 20))
+    money_entry.grid(row=0, column=1)
+    Label(transaction_frame, text="Category of transaction: ", font=("Arial", 20)).grid(row=1, column=0)
+    category_entry = Entry(transaction_frame, font=("Arial", 20))
+    category_entry.grid(row=1, column=1)
+    submit_entry_button = Button(transaction_window, text="Submit transaction",
+                                 command=lambda transact_entry=money_entry, category_entry=category_entry:
+                                 submit_transaction(transact_entry, category_entry, transaction_label, balance_label))
+    submit_entry_button.pack()
 # The menu where the user enters money and sees statistics is opened.
-def accountMenu(username):
+def open_account_menu(username):
+    # The program grams the account's data from its text file.
+    grab_user_data(username)
 
-    #The program grams the account's data from it's text file.
-    grabUserData(username)
-
-    #The program gets the present date.
+    # The program gets the present date.
     today = date.today()
     present_date = today.strftime("%d/%m/%y")
-    checkDate(present_date)
+    check_date(present_date)
 
     # The main menu is destroyed and the account window is opened
     main_menu_window.destroy()
@@ -161,19 +176,28 @@ def accountMenu(username):
     # Displays the currently selected date
     selected_date_label = Label(account_window, text=f"Selected date: {present_date}", font=("Arial", 20))
     selected_date_label.pack()
+    # Displays the current balance of the user.
+    current_balance_label = Label(account_window, text=f"Current balance: {balances.get(selected_account.get())}",
+                                  font=("Arial", 20))
+    current_balance_label.pack()
+    money_transacted_label = Label(account_window, text=(f"Money spent on {present_date}: " + ""), font=("Arial", 20))
+    money_transacted_label.pack()
     # The user can transact money with this button.
-    transaction_button = Button(account_window, text="Transact money", font=("Arial", 25), command=transaction)
+    transaction_button = Button(account_window, text="Transact money", font=("Arial", 25),
+                                command= lambda transact_label=money_transacted_label,
+                                                balance_label=current_balance_label :
+                                open_transaction_menu(transact_label, balance_label))
     transaction_button.pack()
     # The user can choose the date which he wants to log entries in with the calendar.
     calendar_button = Button(account_window, text="Select Day", font=("Arial", 25),
-                             command=lambda label=selected_date_label : openCalendar(label))
+                             command=lambda label=selected_date_label: open_calendar(label))
     calendar_button.pack()
 
     account_window.mainloop()
 
 
 # The user confirms his sign (if his entered account is valid).
-def confirmSignIn(username_entry, password_entry):
+def confirm_sign_in(username_entry, password_entry):
     if len(account_details) == 0:
         Label(sign_in_menu, text="There arent any existing accounts yet!", font=("Arial", 15)).pack()
     # The program goes through each account, and checks if the entered username and password match with any account.
@@ -182,13 +206,12 @@ def confirmSignIn(username_entry, password_entry):
 
         if username_entry.get() == key and password_entry.get() == account_details.get(key):
             current_account.set(username_entry.get())
-            accountMenu(username_entry.get())
+            open_account_menu(username_entry.get())
         else:
-            print("HEllo!")
             Label(sign_in_menu, text="Incorrect username or password!", font=("Arial", 15)).pack()
 
 
-def signIn():
+def open_sign_in_menu():
     global sign_in_menu
     sign_in_menu = Toplevel()
     sign_in_menu.title("Sign In")
@@ -206,11 +229,11 @@ def signIn():
 
     # This button calls the confirm sign in method
     button = Button(sign_in_menu, text="Sign in", font=("Arial", 25),
-                    command=lambda username=username_entry, password=password_entry: confirmSignIn(username, password))
+                    command=lambda username=username_entry, password=password_entry: confirm_sign_in(username, password))
     button.pack()
 
 
-def submitBalance(balance_entry, name_entry):
+def submit_balance(balance_entry, name_entry):
     balances.update({name_entry.get(): balance_entry.get()})
 
     # Puts all the data in account_details in the variable new_account_json, in JSON format, as a string variable.
@@ -228,7 +251,7 @@ def submitBalance(balance_entry, name_entry):
 
 
 # When creating his account, the user enters his starting balance
-def setBalance(name_entry):
+def open_set_balance_menu(name_entry):
     # The balance window is opened.
     global balance_window
     balance_window = Toplevel()
@@ -241,12 +264,12 @@ def setBalance(name_entry):
     balance_entry.pack()
     submit_balance_button = Button(balance_window, text="Submit balance", font=("Arial", 20),
                                    command=
-                                   lambda entry=balance_entry, username=name_entry: submitBalance(entry, username))
+                                   lambda entry=balance_entry, username=name_entry: submit_balance(entry, username))
     submit_balance_button.pack()
 
 
 # The user tries to submit his account
-def submitAccount(username_entry, password_entry, menu):
+def submit_account(username_entry, password_entry, menu):
     # If the user enters a username which is already taken, this is set to false, and the account won't be created.
     account_valid = True
 
@@ -265,11 +288,11 @@ def submitAccount(username_entry, password_entry, menu):
         # The account details variable which contains all the account usernames and passwords is updated.
         account_details.update({username_entry.get(): password_entry.get()})
 
-        setBalance(username_entry)
+        open_set_balance_menu(username_entry)
 
 
 # The create account menu is shown
-def createAccount():
+def open_account_creation_menu():
     # this variable is global since another function needs to access it
     global new_account_menu
 
@@ -290,12 +313,12 @@ def createAccount():
     # With this button, the user tries to submit his account.
     button = Button(new_account_menu, text="Create Account", font=("Arial", 25),
                     command=lambda username=username_entry, password=password_entry:
-                    submitAccount(username, password, new_account_menu))
+                    submit_account(username, password, new_account_menu))
     button.pack()
 
 
 # The menu at the start of running the program is displayed.
 main_menu_window.title("Main Menu")
-Button(main_menu_window, text="Sign in", font=("Arial", 25), command=signIn).pack()
-Button(main_menu_window, text="Create account", font=("Arial", 25), command=createAccount).pack()
+Button(main_menu_window, text="Sign in", font=("Arial", 25), command=open_sign_in_menu).pack()
+Button(main_menu_window, text="Create account", font=("Arial", 25), command=open_account_creation_menu).pack()
 main_menu_window.mainloop()
